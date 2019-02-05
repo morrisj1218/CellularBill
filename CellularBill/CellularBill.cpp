71d20e947bf6e4f84b2e3ec3f411b6b728216983
Jack Morris <morrisj1218@gmail.com>
2019-02-05 02:32:42 -0600

    commit2

--- CellularBill/CellularBill.cpp
+++ CellularBill/CellularBill.cpp
@@ -9,12 +9,15 @@ using namespace std;
 
 int main()
 {
-    // Constants
+	cout << endl;
+	cout << setw(65) << setfill(' ') << right << "Nostalgic Wireless\n" << endl;
+	
+	// Constants
 	const double REG_SERVICE_FEE = 10.00;
 	const double REG_COST_PER_MIN = 0.20;
 	const double PRE_SERVICE_FEE = 25.00;
-	const double PRE_DAY_PER_MIN = 0.10;
-	const double PRE_NIGHT_PER_MIN = 0.05;
+	const double DAY_PER_MIN = 0.10;
+	const double NIGHT_PER_MIN = 0.05;
 	
 	// Declare Variables
 	char serviceCode;
@@ -29,60 +32,129 @@ int main()
 
 	cout << fixed << showpoint << setprecision(2);
 
-	cout << "Enter customer account number: ";
+	cout << "  Enter customer account number: ";
 	cin >> accountNum;
 	cout << endl;
 
-	cout << "Enter service code (R for Regular or P for Premium): ";
+	cout << "  Enter service code (R for Regular or P for Premium): ";
 	cin >> serviceCode;
 	cout << endl;
 
 	switch (serviceCode)
 	{
-	case 'r':
-	case 'R':
-	{
-		cout << "Enter total number of minutes used: ";
-		cin >> reg_Minutes;
-		cout << endl;
-
-		if (reg_Minutes <= 50)
+		// CALCULATE REGULAR
+		case 'r':
+		case 'R':
 		{
-			regAmountDue = REG_SERVICE_FEE;
+			cout << "  Enter total number of minutes used: ";
+			cin >> reg_Minutes;
+			cout << endl;
+
+			if (reg_Minutes <= 50)
+			{
+				regAmountDue = REG_SERVICE_FEE;
+			}
+			else
+			{
+				regAmountDue = REG_SERVICE_FEE + ((reg_Minutes - 50) * REG_COST_PER_MIN);
+			}
+
+			// DISPLAY REGULAR BILL
+			cout << setw(60) << setfill('*') << " " << endl;
+
+			cout << setfill('.') << setw(50) << left << "  Account Number: "
+				<< right << " " << accountNum << endl;
+			cout << setfill('.') << setw(50) << left << "  Type of Service: "
+				<< right << " " << "REGULAR" << endl;
+			cout << setfill('.') << setw(50) << left << "  Number of Minutes Used: "
+				<< right << " " << reg_Minutes << endl;
+			cout << endl;
+		
+			cout << setfill('.') << setw(50) << left << "  Total Amount Due: "
+				<< setfill(' ') << right << " $" << setw(6) << regAmountDue << endl;
+		break;
 		}
-		else
+
+		// CALCULATE PREMIUM
+		case 'p':
+		case 'P':
 		{
-			regAmountDue = REG_SERVICE_FEE + ((reg_Minutes - 50) * REG_COST_PER_MIN);
+			cout << "  Enter total number of minutes used between 6am and 6pm (DAYTIME): ";
+			cin >> preDay_Minutes;
+			cout << endl;
+
+			cout << "  Enter total number of minutes used between 6pm and 6am (EVENING): ";
+			cin >> preNight_Minutes;
+			cout << endl;
+
+			if (preDay_Minutes <= 75)
+			{
+				preDay_Charges = 0;
+			}
+			else
+			{
+				preDay_Charges = (preDay_Minutes - 75) * DAY_PER_MIN;
+			}
+
+			if (preNight_Minutes <= 100)
+			{
+				preNight_Charges = 0;
+			}
+			else
+			{
+				preNight_Charges = (preNight_Minutes - 100) * NIGHT_PER_MIN;
+			}
+
+			preTotal_AmtDue = PRE_SERVICE_FEE + preDay_Charges + preNight_Charges;
+			
+			// DISPLAY PREMIUM BILL
+			cout << setw(60) << setfill('*') << " " << endl;
+
+			cout << setw(50) << setfill('.') << left << "  Account Number: "
+				<< right << " " << accountNum << endl;
+			cout << setw(50) << setfill('.') << left << "  Type of Service: "
+				<< right << " " << "PREMIUM" << endl;
+			cout << setw(50) << setfill('.') << left << "  DAYTIME minutes accured: "
+				<< right << " " << preDay_Minutes << endl;
+			cout << setw(50) << setfill('.') << left << "  EVENING minutes accured: "
+				<< right << " " << preNight_Minutes << endl;
+			cout << setw(50) << setfill('.') << left << "  Service Fee: "
+				<< setfill(' ') << right << " $" << setw(8) << PRE_SERVICE_FEE << endl;
+
+			if (preDay_Minutes <= 75)
+			{
+				cout << setw(50) << setfill('.') << left << "  Charges for DAYTIME minutes: "
+					<< right << " " << "NO CHARGE" << endl;
+			}
+			else
+			{
+				cout << setw(50) << setfill('.') << left << "  Charges for DAYTIME minutes: "
+					<< setfill(' ') << right << " $" << setw(8) << preDay_Charges << endl;
+			}
+
+			if (preNight_Minutes <= 75)
+			{
+				cout << setw(50) << setfill('.') << left << "  Charges for EVENING minutes: "
+					<< right << " " << "NO CHARGE" << endl;
+			}
+			else
+			{
+				cout << setw(50) << setfill('.') << left << "  Charges for EVENING minutes: "
+					<< setfill(' ') << right << " $" << setw(8) << preNight_Charges << endl;
+			}
+			cout << endl;
+
+			cout << setw(50) << setfill('.') << left << "  Total Amount Due: "
+				<< setfill(' ') << right << " $" << setw(8) << preTotal_AmtDue << endl;
+
+			break;
 		}
 
-		cout << "***********************************************************************" << endl;
-		cout << setfill('.') << left << setw(35) << "Account Number: " << right << " " << accountNum << endl;
-		cout << setfill('.') << left << setw(35) << "Type of Service: " << right << " " << serviceCode << endl;
-		cout << setfill('.') << left << setw(35) << "Number of Minutes Used: " << right << " " << reg_Minutes << endl;
-		cout << setfill('.') << left << setw(35) << "Total Amount Due: "
-			<< setfill(' ') << right << " $" << setw(6) << regAmountDue << endl;
-		break;
-	}
-
-	case 'p':
-	case 'P':
-	{
-		cout << "Enter total number of minutes used between 6am and 6pm: ";
-		cin >> preDay_Minutes;
-		cout << endl;
-
-		cout << "Enter total number of minutes used between 6pm and 6am: ";
-		cin >> preNight_Minutes;
-		cout << endl;
+		default:
+		{
 
 		break;
-	}
-
-	default:
-	{
-
-		break;
-	}
+		}
 
 	}
 
